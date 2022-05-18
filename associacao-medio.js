c1f33337604699d60cefef917fc5ce57e335c1d0
mrjgsantos <58551734+mrjgsantos@users.noreply.github.com>
2022-05-18 17:21:47 +0100

    novo

--- associacao-medio.js
+++ associacao-medio.js
@@ -2,129 +2,125 @@ let bases = [];
 let janelas = [];
 let telhados = [];
 let portas = [];
+
 let posXPorta = [];
 let posXJanela = [];
-
-let imgPorta1;
-let imgPorta2;
-let imgBase1;
-let imgBase2;
-let imgJanela1;
-let imgJanela2;
-let imgTelhado1;
-let imgTelhado2;
 
 let randomPorta = [];
 let randomBase = [];
 let randomJanela = [];
 
-let tamanho;
+let tamanho = Math.round(2 + Math.random() * 2);
 let randomPosX;
 let randomAndares;
 
 function preload() {
-  imgPorta1 = loadImage("data/ds.png");
-  imgPorta2 = loadImage("data/ds2.png");
-  imgBase1 = loadImage("data/b1x1.png");
-  imgJanela1 = loadImage("data/wb.png");
-  imgJanela2 = loadImage("data/wb2.png");
+  loadJSON("janelas.json", loadJanelas);
+  loadJSON("portas.json", loadPortas);
+  loadJSON("bases.json", loadBases);
+}
+async function loadJanelas(data) {
+  for (const janela of Object.keys(data)) {
+    let img = await loadImage(data[janela]["imagePath"]);
+    //console.log(img);
+    img.resize(round(img.width / tamanho), 0);
+    data[janela]["imagem"] = img;
+    data[janela]["resizeFactor"] = tamanho;
+    //janelas.push(loadImage(janelaInfo[janela]["ima
+    //janelas.push(loadImage(janelaInfo[janela]["imagem"]));
+    janelas.push(data[janela]);
+  }
+
+  console.log(janelas);
+}
+
+async function loadPortas(data) {
+  console.log(data);
+  console.log("entrei");
+  for (const porta of Object.keys(data)) {
+    let img = await loadImage(data[porta]["imagePath"]);
+    //console.log(img);
+    //console.log("tamanho1:", img.width, tamanho);
+    // img.resize(round(img.width / round(tamanho)), 0);
+    console.log("pos", img);
+    data[porta]["imagem"] = img;
+    data[porta]["resizeFactor"] = round(tamanho);
+    //janelas.push(loadImage(janelaInfo[janela]["imagem"]));
+    portas.push(data[porta]);
+  }
+
+  console.log(portas);
+}
+
+async function loadBases(data) {
+  console.log(data);
+  console.log("entrei");
+  for (const base of Object.keys(data)) {
+    let img = await loadImage(data[base]["imagePath"]);
+    console.log(img);
+    console.log("tamanho1:", img.width, tamanho);
+    // img.resize(round(img.width / round(tamanho)), 0);
+    console.log("pos", img);
+    data[base]["imagem"] = img;
+    data[base]["resizeFactor"] = round(tamanho);
+    //janelas.push(loadImage(janelaInfo[janela]["imagem"]));
+    bases.push(data[base]);
+  }
+
+  console.log(bases);
 }
 
 function setup() {
-  createCanvas(500, 500);
-  tamanho = random(15, 20);
+  createCanvas(windowWidth, windowHeight);
 
-  imgBase1.resize(imgBase1.width / tamanho, imgBase1.height / tamanho);
-  imgPorta1.resize(imgPorta1.width / tamanho, imgPorta1.height / tamanho);
-  imgPorta2.resize(imgPorta2.width / tamanho, imgPorta2.height / tamanho);
-  imgJanela1.resize(imgJanela1.width / tamanho, imgJanela1.height / tamanho);
-  imgJanela2.resize(imgJanela2.width / tamanho, imgJanela2.height / tamanho);
   randomAndares = int(random(2, 10));
 
-  janelas = [
-    {
-      nome: "Botanico",
-      imagem: imgJanela1,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-    {
-      nome: "Botanico",
-      imagem: imgJanela2,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-  ];
-
-  portas = [
-    {
-      nome: "Botanico",
-      imagem: imgPorta1,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-    {
-      nome: "Botanico",
-      imagem: imgPorta2,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-  ];
-
-  bases = [
-    {
-      nome: "Botanico",
-      imagem: imgBase1,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-  ];
-  telhados = [
-    {
-      nome: "Botanico",
-      imagem: imgTelhado1,
-      estilo: "Barroco",
-      altura: "1",
-      largura: "2",
-    },
-  ];
-  randomPosX = random(0, width - imgBase1.width);
   for (let i = 0; i < randomAndares; i++) {
+    randomJanela[i] = random(janelas);
     randomPorta[i] = random(portas);
     randomBase[i] = random(bases);
-
+    randomPosX = random(0, width - randomBase[i].imagem.width);
     posXPorta[i] = random(
       randomPosX,
-      randomPosX + imgBase1.width - imgPorta1.width
+      randomPosX + randomPorta[i].imagem.width - randomPorta[i].imagem.width
     );
     posXJanela[i] = random(
       randomPosX,
-      randomPosX + imgBase1.width - imgJanela1.width
+      randomPosX + randomBase[i].imagem.width - randomJanela[i].imagem.width
     );
-    randomJanela[i] = random(janelas);
+
     print(posXJanela);
+    print(
+      randomPosX + randomJanela[i].imagem.width - randomJanela[i].imagem.width
+    );
   }
-  print(randomPosX + imgJanela1.width - imgJanela1.width);
 }
 
 function draw() {
   background(220);
   for (let i = 0; i < randomAndares; i++) {
     image(randomBase[i].imagem, randomPosX, i * randomBase[i].imagem.height);
+    console.log(randomBase[i]);
     image(
       randomPorta[i].imagem,
+
       posXPorta[i],
       (i + 1) * randomBase[i].imagem.height - randomPorta[i].imagem.height
     );
+
     image(
       randomJanela[i].imagem,
       posXJanela[i],
-      i * randomBase[i].imagem.height + randomBase[i].imagem.height * 0.1
+      i * randomBase[i].imagem.height + randomBase[i].imagem.height * 0.1,
+      randomJanela[i].imagem.width / tamanho,
+      randomJanela[i].imagem.height / tamanho
     );
   }
+}
+
+function keyPressed() {
+  /* for (const j of Object.keys(janelas)) {
+    janelas[j]["resizeFactor"] = random(2, 5);
+  }*/
+  tamanho = random(2, 5);
 }
