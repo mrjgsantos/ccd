1cd491a0c9043dff2fb49b3c386ba79ea476449a
rmurta500 <rmurta500@hotmail.com>
2022-05-20 14:46:49 +0100

    Update associacao-medio.js

--- associacao-medio.js
+++ associacao-medio.js
@@ -7,10 +7,19 @@ let posXJanela = [];
 
 let imgPorta1;
 let imgPorta2;
+let imgPorta3;
+let imgPorta4;
+let imgPorta5;
+let imgPorta6;
 let imgBase1;
 let imgBase2;
 let imgJanela1;
 let imgJanela2;
+let imgJanela3;
+let imgJanela4;
+let imgJanela5;
+let imgJanela6;
+let imgJanela7;
 let imgTelhado1;
 let imgTelhado2;
 
@@ -23,11 +32,20 @@ let randomPosX;
 let randomAndares;
 
 function preload() {
-  imgPorta1 = loadImage("data/ds.png");
-  imgPorta2 = loadImage("data/ds2.png");
+  imgPorta1 = loadImage("data//doors/db_1.png");
+  imgPorta2 = loadImage("data//doors/db_2.png");
+  imgPorta3 = loadImage("data//doors/db_3.png");
+  imgPorta4 = loadImage("data//doors/ds_1.png");
+  imgPorta5 = loadImage("data//doors/ds_2.png");
+  imgPorta6 = loadImage("data//doors/ds_3.png");
   imgBase1 = loadImage("data/b1x1.png");
-  imgJanela1 = loadImage("data/wb.png");
-  imgJanela2 = loadImage("data/wb2.png");
+  imgJanela1 = loadImage("data/windows/wb_1.png");
+  imgJanela2 = loadImage("data/windows/wb_2.png");
+  imgJanela3 = loadImage("data/windows/wb_3.png");
+  imgJanela4 = loadImage("data/windows/ws_1.png");
+  imgJanela5 = loadImage("data/windows/ws_2.png");
+  imgJanela6 = loadImage("data/windows/ws_3.png");
+  imgJanela7 = loadImage("data/windows/ws_4.png");
 }
 
 function setup() {
@@ -37,6 +55,10 @@ function setup() {
   imgBase1.resize(imgBase1.width / tamanho, imgBase1.height / tamanho);
   imgPorta1.resize(imgPorta1.width / tamanho, imgPorta1.height / tamanho);
   imgPorta2.resize(imgPorta2.width / tamanho, imgPorta2.height / tamanho);
+  imgPorta3.resize(imgPorta3.width / tamanho, imgPorta3.height / tamanho);
+  imgPorta4.resize(imgPorta4.width / tamanho, imgPorta4.height / tamanho);
+  imgPorta5.resize(imgPorta5.width / tamanho, imgPorta5.height / tamanho);
+  imgPorta6.resize(imgPorta6.width / tamanho, imgPorta6.height / tamanho);
   imgJanela1.resize(imgJanela1.width / tamanho, imgJanela1.height / tamanho);
   imgJanela2.resize(imgJanela2.width / tamanho, imgJanela2.height / tamanho);
   randomAndares = int(random(2, 10));
@@ -56,6 +78,41 @@ function setup() {
       altura: "1",
       largura: "2",
     },
+    {
+      nome: "Botanico",
+      imagem: imgJanela3,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgJanela4,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgJanela5,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgJanela6,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgJanela7,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
   ];
 
   portas = [
@@ -69,6 +126,34 @@ function setup() {
     {
       nome: "Botanico",
       imagem: imgPorta2,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgPorta3,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgPorta4,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgPorta5,
+      estilo: "Barroco",
+      altura: "1",
+      largura: "2",
+    },
+    {
+      nome: "Botanico",
+      imagem: imgPorta6,
       estilo: "Barroco",
       altura: "1",
       largura: "2",
